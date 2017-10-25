ecd69380c9d6a4c09c086d17fc112cc143e5d853
Frederik Agervold <fwra@me.com>
2017-10-26 00:05:52 +0200

    Added Extension

---
+++ extension/script.js
@@ -0,0 +1,17 @@
+var parser = new DOMParser();
+var totalRating = 0;
+var len = $(".info").length;
+var counter = 0;
+
+$(".info").each(function(i, info) {
+    (function(i, info) {
+        $.get($(info).find("a").attr("href"), function(res) {
+            var doc = parser.parseFromString(res, "text/html");
+            var rating = $(doc).find(".ratingValue strong").text();
+            totalRating += parseFloat(rating.replace(",","."));
+            $($(info.previousElementSibling).find("div")[0]).append(`<div style='top:0'>${rating}</div>`);
+            if (++counter == len) $("#episode_top")[0].innerText += ` - ${(totalRating/$(".info").length).toFixed(1)}`;
+        });
+    })(i, info);
+});
+
